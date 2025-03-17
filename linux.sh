2327d0c1d712d2c423dfa0d236671a868b17a0ae
Georgioct <georgioct@csd.auth.gr>
2025-03-17 12:19:23 +0200

    linux sh

--- linux.sh
+++ linux.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# Define variables
+# Define absolute paths
 PROJECT_DIRECTORY="/home/giorgos/Thesis/src"
 CPLEX_JAR="/home/giorgos/Thesis/lib/cplex.jar"
+INPUT_DIRECTORY="/home/giorgos/Thesis/input"
+OUTPUT_DIRECTORY="/home/giorgos/Thesis/output"
 
-# Change directory to project folder
-cd "$PROJECT_DIRECTORY" || exit
+# Change to project directory
+cd "$PROJECT_DIRECTORY" || { echo "Error: Project directory not found"; exit 1; }
 
 # Compile Java files
 javac -cp ".:$CPLEX_JAR" SAHv1.java
 
-# Run SAHv1 with different input files
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/5/smartpoints100.txt ../input/5/parcels50.txt ../output/linuxOutput/5/solution50.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/5/smartpoints100.txt ../input/5/parcels100.txt ../output/linuxOutput/5/solution100.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/5/smartpoints100.txt ../input/5/parcels150.txt ../output/linuxOutput/5/solution150.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/5/smartpoints200.txt ../input/5/parcels200.txt ../output/linuxOutput/5/solution200.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/10/smartpoints200.txt ../input/10/parcels200.txt ../output/linuxOutput/10/solution200.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/10/smartpoints400.txt ../input/10/parcels400.txt ../output/linuxOutput/10/solution400.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/10/smartpoints500.txt ../input/10/parcels500.txt ../output/linuxOutput/10/solution500.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/20/smartpoints500.txt ../input/20/parcels200.txt ../output/linuxOutput/20/solution200.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/20/smartpoints500.txt ../input/20/parcels400.txt ../output/linuxOutput/20/solution400.txt ../input/MIP.properties
-java -cp ".:$CPLEX_JAR" SAHv1 ../input/20/smartpoints500.txt ../input/20/parcels500.txt ../output/linuxOutput/20/solution500.txt ../input/MIP.properties
+# Run SAHv1 with absolute paths
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/5/smartpoints100.txt" "$INPUT_DIRECTORY/5/parcels50.txt" "$OUTPUT_DIRECTORY/linuxOutput/5/solution50.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/5/smartpoints100.txt" "$INPUT_DIRECTORY/5/parcels100.txt" "$OUTPUT_DIRECTORY/linuxOutput/5/solution100.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/5/smartpoints100.txt" "$INPUT_DIRECTORY/5/parcels150.txt" "$OUTPUT_DIRECTORY/linuxOutput/5/solution150.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/5/smartpoints200.txt" "$INPUT_DIRECTORY/5/parcels200.txt" "$OUTPUT_DIRECTORY/linuxOutput/5/solution200.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/10/smartpoints200.txt" "$INPUT_DIRECTORY/10/parcels200.txt" "$OUTPUT_DIRECTORY/linuxOutput/10/solution200.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/10/smartpoints400.txt" "$INPUT_DIRECTORY/10/parcels400.txt" "$OUTPUT_DIRECTORY/linuxOutput/10/solution400.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/10/smartpoints500.txt" "$INPUT_DIRECTORY/10/parcels500.txt" "$OUTPUT_DIRECTORY/linuxOutput/10/solution500.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/20/smartpoints500.txt" "$INPUT_DIRECTORY/20/parcels200.txt" "$OUTPUT_DIRECTORY/linuxOutput/20/solution200.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/20/smartpoints500.txt" "$INPUT_DIRECTORY/20/parcels400.txt" "$OUTPUT_DIRECTORY/linuxOutput/20/solution400.txt" "$INPUT_DIRECTORY/MIP.properties"
+java -cp ".:$CPLEX_JAR" SAHv1 "$INPUT_DIRECTORY/20/smartpoints500.txt" "$INPUT_DIRECTORY/20/parcels500.txt" "$OUTPUT_DIRECTORY/linuxOutput/20/solution500.txt" "$INPUT_DIRECTORY/MIP.properties"
 
-# Keep the terminal open
+# Keep terminal open
 read -p "Press any key to continue..."
