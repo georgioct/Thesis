d811e0dfe89648ae552efada6cd74a1920937379
Georgioct <georgioct@csd.auth.gr>
2025-03-21 00:21:35 +0200

    t

--- run10_2.bat
+++ run10_2.bat
@@ -7,32 +7,6 @@ REM
 
 cd %PROJECT_DIRECTORY%
 
-javac -cp ".;%CPLEX_JAR%" VRPv1.java
-
-java -cp ".;%CPLEX_JAR%" VRPv1 ../input/10/400/smartPoints400.txt ../input/10/400/p400_1.txt ../output/oneOptimal/10/400/solution1.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv1 ../input/10/400/smartPoints400.txt ../input/10/400/p400_2.txt ../output/oneOptimal/10/400/solution2.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv1 ../input/10/400/smartPoints400.txt ../input/10/400/p400_3.txt ../output/oneOptimal/10/400/solution3.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv1 ../input/10/400/smartPoints400.txt ../input/10/400/p400_4.txt ../output/oneOptimal/10/400/solution4.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv1 ../input/10/400/smartPoints400.txt ../input/10/400/p400_5.txt ../output/oneOptimal/10/400/solution5.txt ../input/MIP.properties
-
-
-javac -cp ".;%CPLEX_JAR%" VRPv2.java
-
-java -cp ".;%CPLEX_JAR%" VRPv2 ../input/10/400/smartPoints400.txt ../input/10/400/p400_1.txt ../output/multipleOptimal/10/400/solution1.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv2 ../input/10/400/smartPoints400.txt ../input/10/400/p400_2.txt ../output/multipleOptimal/10/400/solution2.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv2 ../input/10/400/smartPoints400.txt ../input/10/400/p400_3.txt ../output/multipleOptimal/10/400/solution3.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv2 ../input/10/400/smartPoints400.txt ../input/10/400/p400_4.txt ../output/multipleOptimal/10/400/solution4.txt ../input/MIP.properties
-
-java -cp ".;%CPLEX_JAR%" VRPv2 ../input/10/400/smartPoints400.txt ../input/10/400/p400_5.txt ../output/multipleOptimal/10/400/solution5.txt ../input/MIP.properties
-
-
 javac -cp ".;%CPLEX_JAR%" VRPv3.java
 
 java -cp ".;%CPLEX_JAR%" VRPv3 ../input/10/400/smartPoints400.txt ../input/10/400/p400_1.txt ../output/oneVRP/10/400/solution1.txt ../input/MIP.properties
