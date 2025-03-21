317d835a61006f808893c821881aec05d8d2c64a
Georgioct <georgioct@csd.auth.gr>
2025-03-21 23:30:05 +0200

    results

--- runSSA.bat
+++ runSSA.bat
@@ -10,21 +10,21 @@ cd %PROJECT_DIRECTORY%
 
 javac -cp ".;%CPLEX_JAR%" SSAHv1.java
 
-java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/20/500/smartPoints500.txt ../input/20/200/parcels200.txt ../output/oneSSA/20/solution200.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/20/200/smartPoints200.txt ../input/20/200/parcels200.txt ../output/oneSSA/20/200/solution200.txt ../input/MIP.properties
 
-java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/20/500/smartPoints500.txt ../input/20/400/parcels400.txt ../output/oneSSA/20/solution400.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/20/400/smartPoints400.txt ../input/20/400/parcels400.txt ../output/oneSSA/20/400/solution400.txt ../input/MIP.properties
 
-java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/20/500/smartPoints500.txt ../input/20/500/parcels500.txt ../output/oneSSA/20/solution500.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/20/500/smartPoints500.txt ../input/20/500/parcels500.txt ../output/oneSSA/20/500/solution500.txt ../input/MIP.properties
 
 
 javac -cp ".;%CPLEX_JAR%" SSAHv2.java
 
 
-java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/20/500/smartPoints500.txt ../input/20/200/parcels200.txt ../output/multipleSSA/20/solution200.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/20/200/smartPoints200.txt ../input/20/200/parcels200.txt ../output/multipleSSA/20/200/solution200.txt ../input/MIP.properties
 
-java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/20/500/smartPoints500.txt ../input/20/400/parcels400.txt ../output/multipleSSA/20/solution400.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/20/400/smartPoints400.txt ../input/20/400/parcels400.txt ../output/multipleSSA/20/400/solution400.txt ../input/MIP.properties
 
-java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/20/500/smartPoints500.txt ../input/20/500/parcels500.txt ../output/multipleSSA/20/solution500.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/20/500/smartPoints500.txt ../input/20/500/parcels500.txt ../output/multipleSSA/20/500/solution500.txt ../input/MIP.properties
 
 
 pause
