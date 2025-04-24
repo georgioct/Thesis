439683c33cace85eed7070a093c217c20bc5aaf8
Georgioct <georgioct@csd.auth.gr>
2025-04-24 17:14:48 +0300

    tests for 5 Sps and 50 parcels

--- runSSA.bat
+++ runSSA.bat
@@ -7,16 +7,77 @@ REM
 
 cd %PROJECT_DIRECTORY%
 
+javac -cp ".;%CPLEX_JAR%" VRPv1.java
+
+java -cp ".;%CPLEX_JAR%" VRPv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_1.txt ../output/oneOptimal/5/50/solution1.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_2.txt ../output/oneOptimal/5/50/solution2.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_3.txt ../output/oneOptimal/5/50/solution3.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_4.txt ../output/oneOptimal/5/50/solution4.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_5.txt ../output/oneOptimal/5/50/solution5.txt ../input/MIP.properties
+
 javac -cp ".;%CPLEX_JAR%" VRPv2.java
 
-java -cp ".;%CPLEX_JAR%" VRPv2 ../input/example/smartPoints.txt ../input/example/parcels.txt ../output/example/solutionOptimal.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" VRPv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_1.txt ../output/multipleOptimal/5/50/solution1.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_2.txt ../output/multipleOptimal/5/50/solution2.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_3.txt ../output/multipleOptimal/5/50/solution3.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_4.txt ../output/multipleOptimal/5/50/solution4.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_5.txt ../output/multipleOptimal/5/50/solution5.txt ../input/MIP.properties
+
+javac -cp ".;%CPLEX_JAR%" VRPv3.java
+
+java -cp ".;%CPLEX_JAR%" VRPv3 ../input/5/50/smartPoints50.txt ../input/5/50/p50_1.txt ../output/oneVRP/5/50/solution1.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv3 ../input/5/50/smartPoints50.txt ../input/5/50/p50_2.txt ../output/oneVRP/5/50/solution2.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv3 ../input/5/50/smartPoints50.txt ../input/5/50/p50_3.txt ../output/oneVRP/5/50/solution3.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv3 ../input/5/50/smartPoints50.txt ../input/5/50/p50_4.txt ../output/oneVRP/5/50/solution4.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv3 ../input/5/50/smartPoints50.txt ../input/5/50/p50_5.txt ../output/oneVRP/5/50/solution5.txt ../input/MIP.properties
 
 javac -cp ".;%CPLEX_JAR%" VRPv4.java
 
-java -cp ".;%CPLEX_JAR%" VRPv4 ../input/example/smartPoints.txt ../input/example/parcels.txt ../output/example/solutionVRP.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" VRPv4 ../input/5/50/smartPoints50.txt ../input/5/50/p50_1.txt ../output/multipleVRP/5/50/solution1.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv4 ../input/5/50/smartPoints50.txt ../input/5/50/p50_2.txt ../output/multipleVRP/5/50/solution2.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv4 ../input/5/50/smartPoints50.txt ../input/5/50/p50_3.txt ../output/multipleVRP/5/50/solution3.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv4 ../input/5/50/smartPoints50.txt ../input/5/50/p50_4.txt ../output/multipleVRP/5/50/solution4.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" VRPv4 ../input/5/50/smartPoints50.txt ../input/5/50/p50_5.txt ../output/multipleVRP/5/50/solution5.txt ../input/MIP.properties
+
+javac -cp ".;%CPLEX_JAR%" SSAHv1.java
+
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_1.txt ../output/oneSSA/5/50/solution1.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_2.txt ../output/oneSSA/5/50/solution2.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_3.txt ../output/oneSSA/5/50/solution3.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_4.txt ../output/oneSSA/5/50/solution4.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv1 ../input/5/50/smartPoints50.txt ../input/5/50/p50_5.txt ../output/oneSSA/5/50/solution5.txt ../input/MIP.properties
 
 javac -cp ".;%CPLEX_JAR%" SSAHv2.java
 
-java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/example/smartPoints.txt ../input/example/parcels.txt ../output/example/solutionSSAH.txt ../input/MIP.properties
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_1.txt ../output/multipleSSA/5/50/solution1.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_2.txt ../output/multipleSSA/5/50/solution2.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_3.txt ../output/multipleSSA/5/50/solution3.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_4.txt ../output/multipleSSA/5/50/solution4.txt ../input/MIP.properties
+
+java -cp ".;%CPLEX_JAR%" SSAHv2 ../input/5/50/smartPoints50.txt ../input/5/50/p50_5.txt ../output/multipleSSA/5/50/solution5.txt ../input/MIP.properties
+
 
 pause
